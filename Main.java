dad2a8c5f9d131ed52b6e4a60a899ed70df13084
Andrey13-cyber <69845551+Andrey13-cyber@users.noreply.github.com>
2021-03-22 11:36:06 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,43 @@
+package rabota;
+
+public class Main
+{
+
+
+        public static void main(String[] args) {
+
+            String s="3[xy]4[xyz]";
+            for (int i = 0; i<s.length(); i++)  {
+                if(s.charAt(i)==']') {
+                    int j=i-1;
+                    while(true)  {
+                        if(s.charAt(j)=='[') {
+                            break;
+                        }
+                        j--;
+                    }
+                    int k=j-1;
+                    while(true) {
+                        if(k<0) {
+                            break;
+                        }
+                        int m=(int)s.charAt(k);
+                        if(m<48 || m>57) {
+                            break;
+                        }
+                        k--;
+                    }
+                    k++;
+                    int freq=Integer.parseInt(s.substring(k, j));
+                    String snippet=s.substring(j+1,i);
+                    String temp="";
+
+                    for (int l = 0; l < freq; l++) {
+                        temp+=snippet;
+                    }
+                    s=s.substring(0,k)+temp+s.substring(i+1);
+                }
+            }
+            System.out.println(s);
+        }
+    }
